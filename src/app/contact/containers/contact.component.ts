afa68bb87b0ca361cb42432b30c79b5715f6f04c
Jared <jaredmar07@gmail.com>
2019-10-12 19:22:35 -0500

    - forms

--- src/app/contact/containers/contact.component.ts
+++ src/app/contact/containers/contact.component.ts
@@ -28,7 +28,6 @@ export class ContactComponent extends BasePageComponent implements OnInit {
   submitForm() {
     const body = new HttpParams()
       .set('form-name', 'contact-form')
-      .append('bot-field', this.contact.bot)
       .append('name', this.contact.name)
       .append('email', this.contact.email)
       .append('phone', this.contact.phone)
