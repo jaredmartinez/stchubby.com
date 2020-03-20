a4c3a12cae3da571ebdcb304ac515f3c71d12b1f
Jared <jaredmar07@gmail.com>
2020-03-20 00:06:57 -0500

    - update dates and docs

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,5 @@
 export const AppConstants = Object.freeze({
   BASE_API_URL: 'http://example.com/',
-  GOLF_TOURNAMENT_DATE: new Date(2020, 3, 25, 0, 0, 0, 0)
+  GOLF_TOURNAMENT_DATE: new Date(2020, 8  , 19, 0, 0, 0, 0),
+  DUE_DATE: new Date(2020, 8  , 5, 0, 0, 0, 0)
 });
--- src/app/sponsor/containers/sponsor.component.ts
+++ src/app/sponsor/containers/sponsor.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppConstants} from "../../app.constants";
 
 @Component({
   selector: 'sponsor',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SponsorComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
+
+  sponsorDate: Date = AppConstants.DUE_DATE;
+
+  sponsorDetails = [
+    {title: 'Bronze', price: '$100', sign: '24” x 15” Sign', detail: 'Sign placed on various course tee boxes'},
+    {title: 'Silver', price: '$250', sign: '24” x 30” Sign', detail: 'Sign placed at various premium golf course locations'},
+    {title: 'Gold', price: '$350', sign: '2’ x 6’ Banner', detail: 'Banner placed at various premium locations'},
+    {title: 'Platinum', price: '$450', sign: '4’ x 8’ Banner', detail: 'Banner placed at various premium locations'},
+    {title: 'Ruby', price: '$1,000', sign: '4’ x 8’ Banner', detail: 'Banner placed at entrance'},
+    {title: 'Diamond', price: '$2,000', sign: '4’ x 8’ Banner', detail: 'Banner placed at entrance'},
+    {title: 'Hope Diamond', price: '$5,000', sign: '4’ x 8’ Banner', detail: 'Banner placed at dinner location'},
+  ]
 
 }
