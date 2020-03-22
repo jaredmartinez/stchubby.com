f0524bd16e631bfe3a5406972e51e8244b08cfc7
Jared Martinez <martinez.jared@heb.com>
2020-03-21 21:57:44 -0500

    - changed due date

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,5 @@
 export const AppConstants = Object.freeze({
   BASE_API_URL: 'http://example.com/',
   GOLF_TOURNAMENT_DATE: new Date(2020, 8  , 19, 0, 0, 0, 0),
-  DUE_DATE: new Date(2020, 8  , 5, 0, 0, 0, 0)
+  DUE_DATE: new Date(2020, 8  , 7, 0, 0, 0, 0)
 });
