543bc173592ddaa9c1111fbae48df482c2177e51
Jared Martinez <jaredmar07@gmail.com>
2022-03-05 10:57:24 -0600

    Update sponsor.component.ts

--- src/app/sponsor/containers/sponsor.component.ts
+++ src/app/sponsor/containers/sponsor.component.ts
@@ -18,32 +18,28 @@ export class SponsorComponent implements OnInit {
 
   sponsorDetails = [
     {title: 'Bronze', price: '$100', sign: '24” x 15” Sign', detail: 'Sign placed on various course tee boxes', details: [
-     '24” x 15” Sign',
-     'Sign placed on various course tee boxes'
+     '24” x 15” Sign placed on various course tee boxes'
      ]},
     {title: 'Silver', price: '$250', sign: '24” x 30” Sign', detail: 'Sign placed at various premium golf course locations', details: [
-    '24” x 30” Sign',
-    'Sign placed at various premium golf course locations'
+    '24” x 30” Sign placed at various premium golf course locations'
     ]},
     {title: 'Gold', price: '$350', sign: '2’ x 6’ Banner', detail: 'Banner placed at various premium locations', details: [
-    '2’ x 6’ Banner',
-    'Placed at various premium areas'
+    '2’ x 6’ Banner placed at various premium areas'
     ]},
     {title: 'Platinum', price: '$450', sign: '4’ x 8’ Banner', detail: 'Banner placed at various premium locations', details: [
-    '4’ x 8’ Banner',
-    'Placed at various premium areas'
+    '4’ x 8’ Banner placed at various premium areas'
     ]},
     {title: 'Ruby', price: '$1,000', sign: '4’ x 8’ Banner', detail: 'Banner placed at entrance', details: [
-    '4’ x 8’ Banner',
+    '4’ x 8’ Banner placed at various premium areas',
     'Lunch Table Sign'
     ]},
     {title: 'Diamond', price: '$2,000', sign: '4’ x 8’ Banner', detail: 'Banner placed at entrance', details: [
-    '4’ x 8’ Banner',
+    '4’ x 8’ Banner placed at various premium areas',
     'Scorecard listing',
     'Lunch Table Sign'
     ]},
     {title: 'Hope Diamond', price: '$5,000', sign: '4’ x 8’ Banner', detail: 'Banner placed at dinner location', details: [
-    '4’ x 8’ Banner',
+    '4’ x 8’ Banner placed at Registration / Auction / Lunch area',
     'Team of 4 registration included',
     'Scorecard listing',
     'Lunch Table Sign'
