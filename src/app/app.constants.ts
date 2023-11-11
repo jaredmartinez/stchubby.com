8672d0aea542a0dea34a794de949310361d6873f
Jared Martinez <jaredmar07@gmail.com>
2023-11-11 09:34:27 -0600

    Update app.constants.ts

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,5 @@
 export const AppConstants = Object.freeze({
   BASE_API_URL: 'http://example.com/',
-  GOLF_TOURNAMENT_DATE: new Date(2023, 4, 6, 0, 0, 0, 0),
-  DUE_DATE: new Date(2023, 4, 1, 0, 0, 0, 0)
+  GOLF_TOURNAMENT_DATE: new Date(2024, 4, 4, 0, 0, 0, 0),
+  DUE_DATE: new Date(2024, 3, 29, 0, 0, 0, 0)
 });
